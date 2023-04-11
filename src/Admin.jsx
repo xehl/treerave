085e50dc6b4ab3d65ef5f678fc5524f125a71f59
ehlee95 <eric95lee@gmail.com>
2023-04-11 17:11:02 -0400

    made admin page, add testing mp3s

---
+++ src/Admin.jsx
@@ -0,0 +1,11 @@
+import Background from "./components/Background";
+import Box from "@mui/material/Box";
+
+export default function Admin() {
+  return (
+    <Box>
+      <Background />
+      <div>Admin</div>
+    </Box>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Admin from './Admin';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App/>} />
+        <Route exact path="/admin" element={<Admin/>} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
