0eb47c8811ca35b6af25c97df590ca42a7ac2179
ehlee95 <eric95lee@gmail.com>
2023-04-14 16:09:00 -0400

    trying icecast

--- src/components/HlsPlayer.jsx
+++ src/components/HlsPlayer.jsx
@@ -1,37 +1,37 @@
 import ReactHlsPlayer from 'react-hls-player';
 import React from 'react';
+import { Howl, Howler } from 'howler';
 
 export default function HlsPlayer({ isPlaying }) {
 
   const playerRef = React.useRef();
+  console.log('playerRef', playerRef)
 
-  if (isPlaying) {
-    playVideo();
-  }
+  const streamUrl = 'http://192.168.1.76:8080/revelations.m3u8' //local
 
-  if (!isPlaying) {
-    pauseVideo();
-  }
-
-  function playVideo() {
-    playerRef.current.play();
-  }
-
-  function pauseVideo() {
-    playerRef.current.pause();
-  }
-
-  const streamUrl = 'http://192.168.1.79:8080/revelations.m3u8'
-  console.log('streamUrl', streamUrl)
+  const sound = new Howl({
+    src: [streamUrl],
+    format: ['mp3'], // or 'ogg' if your stream is in OGG format
+    autoplay: true, // play the stream immediately when the component mounts
+  });
 
   return (
-    <ReactHlsPlayer
-      src={streamUrl}
-      playerRef={playerRef}
-      autoPlay={false}
-      controls={true}
-      width="100%"
-      height="auto"
-    /> 
+    // <ReactHlsPlayer
+    //   src={streamUrl}
+    //   playerRef={playerRef}
+    //   autoPlay={false}
+    //   controls={true}
+    //   hlsConfig={{
+    //     liveDurationInfinity: true,
+    //     maxLoadingDelay: 4,
+    //     minAutoBitrate: 0,
+    //     lowLatencyMode: true,
+    //   }}
+    // /> 
+    <div>
+      <p>Status: {sound.playing() ? 'Playing' : 'Stopped'}</p>
+      <p>Title: {sound.title()}</p>
+      <p>Bitrate: {sound._src[0].split(':')[0]}</p>
+    </div>
   )  
 }
